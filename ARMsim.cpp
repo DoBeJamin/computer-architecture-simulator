8ecef5602ddd625161dfb9ed7416e696cafab24d
DoBeJamin <83788679+DoBeJamin@users.noreply.github.com>
2024-06-17 16:03:38 -0500

    basic stuff

---
+++ ARMsim.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+long long int machineCode[1] = {0xffffffff};
+
+int clockTicks = 0;
+int transistorActivations = 0;
+
+bool registry[16][8];
+
+int opcode = 0x0;
+
+void main () {
+    
+
+    while (true) {
+
+        opcode = 5;
+
+
+
+
+
+
+        clockTicks ++;
+
+
+
+
+    }
+
+
+    
+}
